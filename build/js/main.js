bd935f653e560d0f541b0e937d3a884edd319b34
Malik Dixon <mdixon@rap.midco.net>
2015-11-08 11:55:46 -0700

    added and corrected files

--- build/js/main.js
+++ build/js/main.js
@@ -1,8 +1,8 @@
 // Code goes here
 $(document).ready(function(){
 
-  var aboutSection = $('#about-section');
-  var aboutButton = $('.about_btn');
+ $('#intro').addClass('animated fadeIn');
+ $('.intro-content').addClass('animated fadeInDown');
 
   $(function() {
     $('.chart').easyPieChart({
@@ -16,24 +16,51 @@ $(document).ready(function(){
     });
   });
 
-  aboutButton.on('click', function() {
-        TweenMax.fromTo(aboutSection, 3, {opacity: 0}, {opacity: 1} );
-  });
-
-  $(window).scroll(function(event){
-      event.preventDefault();
+  $(window).scroll(function() {
       var y = $(this).scrollTop();
       console.log(y);
-      var aboutTop = aboutSection.offset().top;
 
+      if($(this).scrollTop() > 550) {
 
-      if (aboutTop) {
-        TweenMax.to(aboutSection, 3, {opacity: 1});
+        $('#about-section').addClass('animated fadeIn')
+        $('.about-img').addClass('animated fadeInLeft');
+        $('.about-info').addClass('animated fadeInRight');
+
       }
 
+      if($(this).scrollTop() > 500) {
 
+        $('.type_job1').addClass('animated fadeInLeft');
+        $('.type_job3').addClass('animated fadeInRight');
+        $('.type_job2').addClass('animated fadeInUp');
+
+      }
+
+      if($(this).scrollTop() > 1325) {
+
+         $('.portfolio_title').addClass('animated fadeInDown');
+			   $('.portfolio-site-1').addClass('animated fadeInDown');
+         $('.portfolio-site-2').addClass('animated fadeInUp');
+
+      }
+
+      if($(this).scrollTop() > 2160) {
+
+
+    			$('.skillset_title').addClass('animated fadeInDown');
+    			$('.marketing').addClass('animated fadeInUp');
+    			$('.develop_title').addClass('animated fadeInUp');
+    			$('.develop').addClass('animated fadeInDown');
+      }
+
+      if($(this).scrollTop() > 3220) {
+
+          $('.testimonial').addClass('animated fadeInDown');
+
+      }
 
   });
+
 });
 
 //Skillset Graph
